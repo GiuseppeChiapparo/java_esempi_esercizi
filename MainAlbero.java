557f79fcfe482aa4e141cdeb3ebc43b05c7b0aca
Giuseppe Chiapparo <giuseppe.chiapparo.91@gmail.com>
2026-03-05 10:29:04 +0100

    creazione main classe albero

---
+++ MainAlbero.java
@@ -0,0 +1,9 @@
+public class MainAlbero{
+    public static void main(String args[]){
+        /*chiedi il numero di alberi da mettere nel vettore */
+        /*crea vettore */
+        /*crea oggetti da mettere nel vettore */
+        
+
+    }
+}
