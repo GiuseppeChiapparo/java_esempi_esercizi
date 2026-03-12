6bd857589d17c622d1262caab9fd94103a3a6500
Giuseppe Chiapparo <giuseppe.chiapparo.91@gmail.com>
2026-03-12 10:29:24 +0100

    creazione vettore e stampa vettore

--- MainAlbero.java
+++ MainAlbero.java
@@ -1,8 +1,29 @@
 public class MainAlbero{
     public static void main(String args[]){
+        Scanner tastiera = new Scanner(System.in);
         /*chiedi il numero di alberi da mettere nel vettore */
-        /*crea vettore */
+        int n;
+        do {
+            System.out.print("Quanti alberi vuoi creare?");
+            n = tastiera.nextInt();
+        } while(n <= 0);
+
+        /*crea vettore che contiene oggetti di tipo Albero*/
+        // int voti[] = new int[n];
+        Albero alberi[] = new Albero[n];
         /*crea oggetti da mettere nel vettore */
+        for(int i = 0; i < n; i++) {
+            Albero a = new Albero();
+            a.inserisciDati(tastiera);
+            alberi[i] = a;
+        }
+        //stampare il vettore
+        System.out.println("Il contenuto del vettore");
+        for (int i = 0; i < n; i++) {
+            System.out.println( alberi[i].toString() );
+        }
+
+        
         
 
     }
